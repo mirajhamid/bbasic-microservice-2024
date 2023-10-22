09db881f64ff44f718e505cdbeee236980ebdc14
miraj.hamid <miraj.hamid@gamesys.co.uk>
2023-10-22 10:21:28 +0100

    initial commit

---
+++ src/main/java/miraj/basicmicroservice2024/BasicMicroservice2024Application.java
@@ -0,0 +1,13 @@
+package miraj.basicmicroservice2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroservice2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroservice2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/miraj/basicmicroservice2024/BasicMicroservice2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package miraj.basicmicroservice2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMicroservice2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
